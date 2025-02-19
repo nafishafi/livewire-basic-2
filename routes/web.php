2ee4e15ed6acb3ff04103de250696d40a2fdcdfd
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-19 09:21:17 +0700

    finished - 5

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//posts index
+Route::get('/', App\Livewire\Posts\Index::class)->name('posts.index');
